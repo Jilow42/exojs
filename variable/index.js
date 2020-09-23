7021898ed9cfae849900e6aa56611c0123b7dcfd
Flo <florian-lina@outlook.fr>
2020-09-23 17:45:34 +0200

    feat(condition): add condition exercices

--- variable/index.js
+++ variable/index.js
@@ -82,3 +82,89 @@ var temp = a;
 a = b;
 b = temp;
 console.log(a, b);
+
+/**
+ * Les conditions
+ */
+
+// 1.
+
+var valeur = prompt('Votre age');
+if (valeur < 18) {
+  alert("Vous ne pouvez pas entrez vous n’êtes pas majeur vous avez ".concat(valeur));
+} else if (valeur >= 42) {
+  alert("Vous pouvez entrer vous êtes majeur vous avez ".concat(valeur).concat(" et vous êtes le BOSS"));
+} else {
+  alert("Vous pouvez entrer vous êtes majeur vous avez ".concat(valeur));
+}
+
+// 2.
+var rand = [Math.floor((Math.random() * 31))];
+if (rand <= 10) {
+  console.log('Cool')
+} else if (rand >= 21) {
+  console.log('Warm')
+} else {
+  console.log('Tepid')
+}
+
+// 3.
+var date = new Date();
+var today = date.getDay();
+console.log(today);
+switch (today) {
+  case 1:
+    alert('nous sommes Lundi');
+    break;
+  case 2:
+    alert('nous sommes Mardi');
+    break;
+  case 3:
+    alert('nous sommes Mercredi');
+    break;
+  case 4:
+    alert('nous sommes Jeudi');
+    break;
+  case 5:
+    alert('nous sommes Vendredi');
+    break;
+  case 6:
+    alert('nous sommes Samedi et c\'est le Weekend');
+    break;
+  case 7:
+    alert('nous sommes Dimanche et c\'est le Weekend');
+    break;
+}
+
+// 4.
+var plop = prompt('Pour commencer votre histoire entrez un mot');
+if (plop === 'plop') {
+  var hist = prompt("C'est le début vous préférez");
+  if (hist === '42') {
+    window.alert('La grande réponse sur la vie, l’univers et le reste !');
+  } else if (hist === 'Stonks') {
+    window.alert('Vous êtes milliardaire');
+  } else if (hist == '12') {
+    window.alert('Noice try Bro\'')
+  } else {
+    prompt('Vous êtes mort')
+  }
+} else {
+  window.alert('Vous êtes mort');
+}
+
+// 5.
+//Pas possible car pas dans une fonction
+
+// 6.
+var truc;
+var tern = truc === undefined ? 'cette variable n\’existe pas' : '42';
+alert(tern);
+
+// 7.
+var familly = (Number(prompt('Choisir un nombre entre 0 et 50')));
+console.log(familly)
+switch (familly)
+    case < 10:
+  console
+} 
