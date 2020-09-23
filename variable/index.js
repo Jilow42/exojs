0fd754b0da6760b4c475fc184a0a4cd5b45bdf78
Flo <florian-lina@outlook.fr>
2020-09-23 12:33:06 +0200

    feat(exercice): end of correction and add missing console log

--- variable/index.js
+++ variable/index.js
@@ -39,6 +39,7 @@ console.log(check);
 
 // 6.
 var myArray42 = ["q", "u", "a", "r", "a", "n", "t", "e", "-", "d", "e", "u", "x"];
+console.log(myArray42);
 
 // 7.
 var myArray42Len = myArray42.length;
@@ -46,37 +47,38 @@ console.log(myArray42Len)
 
 // 8.
 var concat = 'La grande réponse sur la vie, l’univers et le reste !'.concat(myArray42.join(""));
+console.log(concat);
 
 // 9.
 var rand = [Math.floor((Math.random() * 42) + 1)];
 console.log(rand, rand.includes(42));
 
 // 10.
-var my42Type = (variable) => (typeof variable).toString();
-console.log(my42Type(string));
-console.log(my42Type(number));
-console.log(my42Type(float));
-console.log(my42Type(boolean));
-console.log(my42Type(array));
-console.log(my42Type(date));
-console.log(my42Type(objet));
-console.log(my42Type(dontKnow));
-console.log(my42Type(none));
+/**
+ * var my42Type = (variable) => (typeof variable).toString();
+console.log(my42Type(string, number, float, boolean, array, date, objet, dontKnow, none));
+ */
+var my42Type = [typeof string, typeof number, typeof function () { }, typeof objet];
+console.log(my42Type);
 
 // 11.
-var compute42 = 6 * 7
+var compute42 = 6 * 7;
 compute42.toString();
 console.log(compute42);
 
 // 12.
-var stonks = (42424242).toString().split('42').join('quarante-deux ');
+/**
+ * var stonks = (42424242).toString().split('42').join('quarante-deux ');
 console.log(stonks)
+ */
+var stonks = "4242424242";
+stonks = stonks.replace(/42/gi, 'quarante-deux ');
+console.log(stonks);
 
 // 13.
 var a = 24;
 var b = 42;
-var temp = 0;
-temp = a;
+var temp = a;
 a = b;
 b = temp;
 console.log(a, b);
